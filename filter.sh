391ac7654afaee354221c43b9fd61f3c2df112f7
cashcat96 <cashcat96@yahoo.com>
2019-11-01 09:19:47 -0700

    remove operableindexhtml
    because we now use 'cd' in blondedchecker.sh, no longer need to reference absolute file location

--- filter.sh
+++ filter.sh
@@ -3,11 +3,9 @@
 # THIS SCRIPT FILTERS OUT LINES THAT CHANGE FREQUENTLY ON BLONDED.CO
 # if you find another line that comes up constantly, simply add another line, using the other lines as  examples
 
-operable_indexhtml="/home/pi/publicblondedchecker/BlondedChecker/index.html"		# set the path to index.html, so we can remove troublesome lines
-
-sed -i '/token/d' $operable_indexhtml	# remove all lines with the word 'token' in them
-sed -i '/reqid/d' $operable_indexhtml	# remove all lines with the word 'reqid' in them
-sed -i '/Token/d' $operable_indexhtml	# remove all lines with the word 'Token' in them
-sed -i '/sha256/d' $operable_indexhtml   # remove all lines with the word 'sha256' in them
+sed -i '/token/d' index.html	# remove all lines with the word 'token' in them
+sed -i '/reqid/d' index.html	# remove all lines with the word 'reqid' in them
+sed -i '/Token/d' index.html	# remove all lines with the word 'Token' in them
+sed -i '/sha256/d' index.html   # remove all lines with the word 'sha256' in them
 
 #echo 'filter.sh has run'	# uncomment this line to help figure out where problems are occuring
